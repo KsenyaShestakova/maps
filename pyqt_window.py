6055e53ca8d20e30f5a33771c43bb96dee7cc9bf
Дарья <dariiapotapova@yandex.ru>
2022-02-12 16:23:52 +0500

    add pyqt norm

--- pyqt_window.py
+++ pyqt_window.py
@@ -6,7 +6,11 @@ class Data(Ui_MainWindow, QMainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.initUi()
+
+    def initUi(self):
+        self.pushButton.clicked.connect(self.do)
+
+    def do(self):
         self.ll = f'{self.input_d.text()},{self.input_sh.text()}'
         self.sc = self.scale.currentText()
-        if self.ll and self.sc:
-            self.pushButton.clicked.connect()
